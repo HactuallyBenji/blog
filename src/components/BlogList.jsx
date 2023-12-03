5170bb7bf77d7aa9035611ba03654d298f087cd9
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:50:32 -0800

    Use images, and make a grid to view more posts

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import BlogPost from './BlogPost';
+import React from "react";
+import BlogPost from "./BlogPost";
 
 const BlogList = ({ posts, onClick }) => (
-  <div>
-    {posts.map(post => (
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    {posts.map((post) => (
       <BlogPost key={post.id} post={post} onClick={onClick} />
     ))}
   </div>
