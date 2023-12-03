fbf49d9c13a35f6c47ad05f4afc555ef4efadef3
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:50:00 -0800

    Match simple text of the footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => (
   <header className="bg-gray-800 text-white p-4">
-    <h1 className="text-xl font-bold">My Personal Blog</h1>
+    <h1 className="text-xl font-bold">Ben Miller</h1>
   </header>
 );
 
