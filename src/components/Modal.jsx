5c4ea2f3a5d26850ce896eae9fc3508b3782f0c1
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 15:33:17 -0800

    Use childen to make modal content more flexible

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Modal = ({ post, onClose }) => (
+const Modal = ({ onClose, children }) => (
   <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
     <div className="bg-white overflow-auto w-full h-full text-center">
       <button
@@ -9,17 +9,7 @@ const Modal = ({ post, onClose }) => (
       >
         Close
       </button>
-      {/* Centered & smaller image */}
-      <img
-        src={post.thumbnail}
-        alt={post.title}
-        onClick={onClose}
-        className="inline-block w-1/2 sm:w-1/3 md:w-1/4 h-auto mx-auto rounded-lg border m-6 hover:cursor-pointer"
-      />
-      <div className="text-left">
-        <h2 className="text-2xl font-bold my-4">{post.title}</h2>
-        <p>{post.content}</p>
-      </div>
+      {children}
     </div>
   </div>
 );
