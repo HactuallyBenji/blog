fb30bda4df3cc5d7ca95149acb4b9fceda583409
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:23:24 -0800

    Need some placeholder data

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const posts = [
+  { id: 1, title: 'First Post', excerpt: 'This is the first post.', content: 'Full content of the first post.' },
+  { id: 2, title: 'Second Post', excerpt: 'This is the second post.', content: 'Full content of the second post.' },
+  // Add more posts here
+];
+
+export default posts;
