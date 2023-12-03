956efa3ac2ad3d97e35797c19d4321fc0380c20a
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 18:43:56 -0800

    More columns for more square look

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import BlogPost from "./BlogPost";
 
 const BlogList = ({ posts, onClick }) => (
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+  <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
     {posts.map((post) => (
       <BlogPost key={post.id} post={post} onClick={onClick} />
     ))}
