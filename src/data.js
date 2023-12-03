75e8554d58f36280502131eae38a23898ed04bcd
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:49:00 -0800

    Images are nice

--- src/data.js
+++ src/data.js
@@ -1,6 +1,19 @@
 const posts = [
-  { id: 1, title: 'First Post', excerpt: 'This is the first post.', content: 'Full content of the first post.' },
-  { id: 2, title: 'Second Post', excerpt: 'This is the second post.', content: 'Full content of the second post.' },
+  {
+    id: 1,
+    title: "First Post",
+    excerpt: "This is the first post.",
+    content: "Full content of the first post about linear regression.",
+    thumbnail: "/images/linear_regression.png",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    excerpt: "This is the second post.",
+    content:
+      "Full content of the second post about multi-class classification.",
+    thumbnail: "/images/multi-class_classification.png",
+  },
   // Add more posts here
 ];
 
