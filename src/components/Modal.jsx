e65a5f44db8068a56b662e876778d0e1564200ee
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 18:07:10 -0800

    Make sure close button is always visible, on now full screen modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Modal = ({ post, onClose }) => (
-  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center p-4">
-    <div className="bg-white p-4 rounded-md overflow-auto max-w-full max-h-full">
-      <button className="bg-red-500 text-white p-2 rounded" onClick={onClose}>
+  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+    <div className="bg-white p-4 rounded-md overflow-auto w-full h-full">
+      <button
+        className="fixed bg-red-500 text-white p-2 rounded"
+        onClick={onClose}
+      >
         Close
       </button>
       <img src={post.thumbnail} alt={post.title} className="w-full h-auto" />
