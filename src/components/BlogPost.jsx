82afb5c39a8a34e0bb6745adb28112969dc4604d
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:51:03 -0800

    Add images to posts for color and style

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const BlogPost = ({ post, onClick }) => (
-  <div className="border-b border-gray-300 p-4 cursor-pointer" onClick={() => onClick(post)}>
-    <h2 className="text-2xl font-bold">{post.title}</h2>
+  <div
+    className="border border-gray-300 p-4 cursor-pointer"
+    onClick={() => onClick(post)}
+  >
+    <img
+      src={post.thumbnail}
+      alt={post.title}
+      className="w-full h-40 object-cover mb-2"
+    />
+    <h2 className="text-xl font-bold mb-2">{post.title}</h2>
     <p className="text-gray-700">{post.excerpt}</p>
   </div>
 );
