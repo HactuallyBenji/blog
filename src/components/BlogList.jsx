50cae21f9e08a1dafc7b6ae225f04ad201665a67
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 08:51:09 -0800

    Bloglist to show Linear regression and remaining posts

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import BlogPost from "./BlogPost";
+import LinearRegressionPreview from "./LinearRegressionPreview";
 
-const BlogList = ({ posts, onClick }) => (
+const BlogList = ({ posts, onClick, onLinearRegressionClick }) => (
   <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <LinearRegressionPreview onClick={onLinearRegressionClick} />
     {posts.map((post) => (
       <BlogPost key={post.id} post={post} onClick={onClick} />
     ))}
