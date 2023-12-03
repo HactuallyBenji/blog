8f680a131ea0bfc24a11f23dd7fee46a7013d0ae
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 15:30:33 -0800

    To have custom posts, we can't iterate over a generic structure

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BlogPost from "./BlogPost";
 import LinearRegressionPreview from "./LinearRegressionPreview";
+import MultiClassClassificationPreview from "./MultiClassClassificationPreview";
 
-const BlogList = ({ posts, onClick, onLinearRegressionClick }) => (
+const BlogList = ({ onPostClick }) => (
   <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-    <LinearRegressionPreview onClick={onLinearRegressionClick} />
-    {posts.map((post) => (
-      <BlogPost key={post.id} post={post} onClick={onClick} />
-    ))}
+    <LinearRegressionPreview onClick={() => onPostClick("linearRegression")} />
+    <MultiClassClassificationPreview
+      onClick={() => onPostClick("multi-classClassification")}
+    />
   </div>
 );
 
