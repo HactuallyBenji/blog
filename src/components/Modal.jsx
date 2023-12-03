3e4cd63894df1a63072e6133dff7c8f80fee5052
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 18:44:33 -0800

    Close by image for ease

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,16 +2,24 @@ import React from "react";
 
 const Modal = ({ post, onClose }) => (
   <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-    <div className="bg-white p-4 rounded-md overflow-auto w-full h-full">
+    <div className="bg-white overflow-auto w-full h-full text-center">
       <button
-        className="fixed bg-red-500 text-white p-2 rounded"
+        className="bg-red-500 text-white p-2 rounded absolute top-4 right-4"
         onClick={onClose}
       >
         Close
       </button>
-      <img src={post.thumbnail} alt={post.title} className="w-full h-auto" />
-      <h2 className="text-2xl font-bold">{post.title}</h2>
-      <p>{post.content}</p>
+      {/* Centered & smaller image */}
+      <img
+        src={post.thumbnail}
+        alt={post.title}
+        onClick={onClose}
+        className="inline-block w-1/2 sm:w-1/3 md:w-1/4 h-auto mx-auto rounded-lg border m-6 hover:cursor-pointer"
+      />
+      <div className="text-left">
+        <h2 className="text-2xl font-bold my-4">{post.title}</h2>
+        <p>{post.content}</p>
+      </div>
     </div>
   </div>
 );
