f355cfa4277f1dfadbc9a68e2f592d0016cca40c
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:49:46 -0800

    Update to just my name for clean aesthetic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
   <footer className="bg-gray-800 text-white p-4 text-center">
-    <p>© 2023 My Personal Blog</p>
+    <p>© 2023 Ben Miller</p>
   </footer>
 );
 
