cacdf240a5808259b3dece8172f511e0b453a440
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 08:50:48 -0800

    Add temp state management for lin reg. post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import BlogList from "./components/BlogList";
+import LinearRegressionPost from "./components/LinearRegressionPost";
 import Modal from "./components/Modal";
 import Footer from "./components/Footer";
 import posts from "./data";
@@ -8,22 +9,33 @@ import { useState } from "react";
 
 function App() {
   const [selectedPost, setSelectedPost] = useState(null);
+  const [showLinearRegression, setShowLinearRegression] = useState(false);
 
-  const openModal = (post) => {
+  const openPost = (post) => {
     setSelectedPost(post);
+  };
+
+  const openLinearRegression = () => {
+    setShowLinearRegression(true);
   };
 
   const closeModal = () => {
     setSelectedPost(null);
+    setShowLinearRegression(false);
   };
 
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow p-4">
-        <BlogList posts={posts} onClick={openModal} />
+        <BlogList
+          posts={posts}
+          onClick={openPost}
+          onLinearRegressionClick={openLinearRegression}
+        />
       </main>
       {selectedPost && <Modal post={selectedPost} onClose={closeModal} />}
+      {showLinearRegression && <LinearRegressionPost />}
       <Footer />
     </div>
   );
