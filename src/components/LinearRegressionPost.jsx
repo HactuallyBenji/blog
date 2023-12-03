70ef910139e3f7fc8dd73ce174ef8315ef81766c
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 15:31:40 -0800

    Use new header image component

--- src/components/LinearRegressionPost.jsx
+++ src/components/LinearRegressionPost.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import LinearRegressionGraph from "./LinearRegressionGraph";
+import HeaderImage from "./HeaderImage";
 
-const LinearRegressionPost = () => {
+const LinearRegressionPost = ({ onClose }) => {
   return (
     <div>
+      <HeaderImage
+        imgSource="./images/linear_regression.png"
+        alt="Linear Regression"
+        onClose={onClose}
+      />
       <h1>Understanding Linear Regression</h1>
       <p>... Your introduction and explanation ...</p>
       <LinearRegressionGraph />
