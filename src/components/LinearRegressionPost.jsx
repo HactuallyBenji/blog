f2bdf994e435d6b56a56c07a9655ce72b66f17cc
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 08:50:02 -0800

    Need linear regression post

---
+++ src/components/LinearRegressionPost.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LinearRegressionGraph from "./LinearRegressionGraph";
+
+const LinearRegressionPost = () => {
+  return (
+    <div>
+      <h1>Understanding Linear Regression</h1>
+      <p>... Your introduction and explanation ...</p>
+      <LinearRegressionGraph />
+      <p>... Further explanation ...</p>
+    </div>
+  );
+};
+
+export default LinearRegressionPost;
