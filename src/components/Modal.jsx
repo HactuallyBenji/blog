9a6e4b8a5c3a30cbb4bae2150e0488a5016388a0
hactuallybenji <mrbenamiller@gmail.com>
2023-12-02 17:49:22 -0800

    Should use images in the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ post, onClose }) => (
-  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-    <div className="bg-white p-4 rounded-md">
-      <button className="bg-red-500 text-white p-2 rounded" onClick={onClose}>Close</button>
+  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center p-4">
+    <div className="bg-white p-4 rounded-md overflow-auto max-w-full max-h-full">
+      <button className="bg-red-500 text-white p-2 rounded" onClick={onClose}>
+        Close
+      </button>
+      <img src={post.thumbnail} alt={post.title} className="w-full h-auto" />
       <h2 className="text-2xl font-bold">{post.title}</h2>
       <p>{post.content}</p>
     </div>
