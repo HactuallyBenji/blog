c7e77902789edaf3e6a15c4d49e06c8b10059c44
hactuallybenji <mrbenamiller@gmail.com>
2023-12-03 16:45:05 -0800

    Make sure the outer component has a defined height for relative sizing inside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Modal = ({ onClose, children }) => (
-  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-    <div className="bg-white overflow-auto w-full h-full text-center">
+  <div className="fixed inset-0 bg-black h-screen bg-opacity-50 z-50 flex justify-center items-center">
+    <div className="bg-white overflow-auto w-full text-center">
       <button
         className="bg-red-500 text-white p-2 rounded absolute top-4 right-4"
         onClick={onClose}
