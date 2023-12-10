83fb9141eb18f2549b3900bae92f2aabc9320d80
hactuallybenji <mrbenamiller@gmail.com>
2023-12-09 17:34:17 -0800

    Update data to be accurate

--- src/data.js
+++ src/data.js
@@ -1,11 +1,13 @@
 const posts = [
   {
     id: 1,
-    title: "First Post",
-    excerpt: "This is the first post.",
+    title: "Understanding Linear Regression",
+    excerpt: "An exploration of linear regression in data science",
     content:
       "Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. Full content of the first post about linear regression. ",
     thumbnail: "/images/linear_regression.png",
+    frame: "./frames/Ad_click_ab_test.html",
+    youtube: "https://www.youtube.com/embed/wDE6o8VHA0Q?si=fjdibp7avec3UwpH",
   },
   {
     id: 2,
@@ -14,6 +16,17 @@ const posts = [
     content:
       "Full content of the second post about multi-class classification.",
     thumbnail: "/images/multi-class_classification.png",
+    frame: "./frames/Ad_click_ab_test.html",
+    youtube: "https://www.youtube.com/embed/wDE6o8VHA0Q?si=fjdibp7avec3UwpH",
+  },
+  {
+    id: 3,
+    title: "An AB Test Analysis",
+    excerpt: "An exploration of AB test analysis in data science",
+    content: "This is content that we might not need, not sure yet",
+    thumbnail: "image/Ad_click_ab_test.html",
+    frame: "./frames/Ad_click_ab_test.html",
+    youtube: "https://www.youtube.com/embed/wDE6o8VHA0Q?si=fjdibp7avec3UwpH",
   },
   // Add more posts here
 ];
