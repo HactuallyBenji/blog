efd78f4447794bd34f101deff9dd26d1d0ee8316
hactuallybenji <mrbenamiller@gmail.com>
2024-01-21 15:39:18 -0800

    Make sure we have key props for React

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -6,7 +6,11 @@ const BlogList = ({ onPostClick }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
       {posts.map((post) => (
-        <Preview post={post} onClick={() => onPostClick(post.id)} />
+        <Preview
+          key={post.id}
+          post={post}
+          onClick={() => onPostClick(post.id)}
+        />
       ))}
     </div>
   );
