3d4ace1c4c13b1b853ee6f99c7615dceb71bfc4e
hactuallybenji <mrbenamiller@gmail.com>
2024-01-21 15:29:32 -0800

    Dynamically create Previews based on posts data

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import LinearRegressionPreview from "./LinearRegressionPreview";
-import MultiClassClassificationPreview from "./MultiClassClassificationPreview";
-import ABTestAnalysisPreview from "./ABTestAnalysisPreview";
-import SoftmaxRegressionPreview from "./SoftmaxRegressionPreview";
+import Preview from "./Preview.jsx";
 import posts from "../data.js";
 
 const BlogList = ({ onPostClick }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-      <LinearRegressionPreview
-        post={posts[0]}
-        onClick={() => onPostClick("linearRegression")}
-      />
-      <MultiClassClassificationPreview
-        post={posts[1]}
-        onClick={() => onPostClick("multi-classClassification")}
-      />
-      <ABTestAnalysisPreview
-        post={posts[2]}
-        onClick={() => onPostClick("ABTestAnalysis")}
-      />
-      <SoftmaxRegressionPreview
-        post={posts[3]}
-        onClick={() => onPostClick("softmaxRegression")}
-      />
+      {posts.map((post) => (
+        <Preview post={post} onClick={() => onPostClick(post.id)} />
+      ))}
     </div>
   );
 };
---
+++ src/components/Preview.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Preview = ({ post, onClick }) => (
+  <div className="border border-gray-300 p-4 cursor-pointer" onClick={onClick}>
+    <img
+      src={post.thumbnail}
+      alt={post.title}
+      className="w-full h-40 object-cover mb-2"
+    />
+    <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+    <p className="text-gray-700">{post.excerpt}</p>
+  </div>
+);
+
+export default Preview;
