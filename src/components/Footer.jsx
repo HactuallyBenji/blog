5998efafc6667f9adb5b22e483eba4d796ab7c81
hactuallybenji <mrbenamiller@gmail.com>
2024-01-21 15:41:53 -0800

    Update copyright to 2024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer className="bg-gray-800 text-white p-4 text-center">
-    <p>© 2023 Ben Miller</p>
+    <p>© 2024 Ben Miller</p>
   </footer>
 );
 
